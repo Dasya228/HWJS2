f8393ee49ce33b3b092b3443fedb90c7d2aa3a56
Пользователь <world.worensi@gmail.com>
2023-10-26 17:18:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+// Hello ivan
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = a.slice(0,1).toUpperCase() + a.slice(1) + ' ' + b.slice(0,1).toLowerCase() + b.slice(1)
+// console.log(c)
+
+
+
+
+// записать длину в переменную b
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+// console.log(b)
+
+
+// вернуть sit lorem ipsum dolor amet
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice(18,21) + ' ' + a.slice(0,17) + a.slice(21)
+// console.log(b)
+
+
+//вернуть CHILDREn
+// let a = 'children'
+// let b = a.slice(0,7).toUpperCase()+a.slice(7)
+// console.log(b)
+
+
+// вернуть bsso,
+//  let a = 'boss'
+// let b = a.slice(0,1) + a.slice(2) + a.slice(1,2)
+// console.log(b)
+
+
+// вернуть index d
+//  let a = 'sunday'
+// let b = a.indexOf('d')
+// console.log(b)
+
+// вернуть hekso
+//  let a = 'hello'
+// let b = a.replace('ll','ks')
+// console.log(b)
+
+
+// вернуть new us
+// let a = 'user'
+// let b = 'new'
+// let c = a.slice(0,2)
+// let result= b + ' ' +c
+// console.log(result)
+
+
+// вернуть daydaymon
+// let a = 'monday'
+// let b = 'day'
+// let c = b+b+a.slice(0,3)
+// console.log(c)
+
+
+
+// вернуть 'hElLo'
+// let a = 'HELLO'
+// let b =  a.slice(0,1).toLowerCase() + a.slice(1,2) + a.slice(2,3).toLowerCase() + a.slice(3,4) + a.slice(4,5).toLowerCase()
+// console.log(b)
+
+
+
+// вернуть '450'
+// let a = 5
+//  let b = '400'
+// let c = b.slice(0,1) + a + b.slice(1,2)
+// console.log(c)
+
+// вернуть manak
+//  let a = 'monkey'
+//  let b = 'banana'
+// let c = a.slice(0,1) + b.substr(1,3)+a.slice(3,4)
+// console.log(c)
+//
+//
+
+
